b6ad9c71d28d2a542a199b15cbd037a44c839cb4
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-01-06 15:11:08 +0700

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Putut Yusri Bahtiar on 06/01/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
